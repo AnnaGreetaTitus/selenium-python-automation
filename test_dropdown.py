2a6d87362ad2a9a78df216a3c9b85c9dc7345ac6
AnnaGreetaTitus <annagretus@gmail.com>
2026-04-22 14:37:35 +0530

    Add files via upload

---
+++ test_dropdown.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/dropdown")
+
+
+dropdown = Select(driver.find_element(By.XPATH, "//select[@id = 'dropdown']"))
+dropdown.select_by_visible_text("Option 1")
+
+time.sleep(5)
+driver.quit()
