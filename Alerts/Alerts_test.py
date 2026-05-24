19f0b98e4ecccc33964a2a03ece33e56241c33b0
Anna Greeta Titus <annagretus@gmail.com>
2026-05-24 19:48:03 +0530

    Add files via upload

---
+++ Alerts/Alerts_test.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+
+driver.find_element(By.XPATH, "//button[normalize-space()='Click for JS Alert']").click()
+alert = driver.switch_to.alert
+print(alert.text)
+alert.accept()
+
+driver.find_element(By.XPATH, "//button[normalize-space()='Click for JS Confirm']").click()
+alert = driver.switch_to.alert
+print(alert.text)
+alert.dismiss()
+
+time.sleep(10)
+driver.quit()
+
